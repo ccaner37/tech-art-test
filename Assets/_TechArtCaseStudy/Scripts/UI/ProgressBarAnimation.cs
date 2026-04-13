3e1209110c2ba765543376781679ae1ee91b0bd1
ccaner37 <ccaner37@hotmail.com>
2026-04-13 18:05:41 +0300

    Text animation

--- Assets/_TechArtCaseStudy/Scripts/UI/ProgressBarAnimation.cs
+++ Assets/_TechArtCaseStudy/Scripts/UI/ProgressBarAnimation.cs
@@ -2,6 +2,7 @@ using Appodeal.TechartCaseStudy.AudioEffect;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace AppoDeal.TehartCaseStudy.UI
 {
@@ -9,42 +10,67 @@ namespace AppoDeal.TehartCaseStudy.UI
     {
         [SerializeField] private Button _button;
         [SerializeField] private Slider _progressBar;
+        [SerializeField] private TextMeshProUGUI _progressText;
 
         private const float FillDuration = 0.6f;
+        private const float StartValue = 650f;
+        private const float EndValue = 2000f;
 
         private void Start()
         {
             _button.onClick.AddListener(OnPlayClicked);
+            UpdateText(StartValue);
         }
 
         public void OnPlayClicked()
         {
-            _progressBar.value = 0.075f; // Hardcoded only for testing
-
+            _progressBar.value = 0.075f; // Hardcoded for testing purposes
             _button.interactable = false;
 
             AudioService.Instance.PlaySound(GameAudioType.ButtonClick);
 
             Sequence sequence = DOTween.Sequence();
 
-            // 2. BUTON ANİMASYONU
-            //_playButton.transform.DOPunchScale(Vector3.one * -0.1f, 0.3f, 10, 1);
-
-            sequence.InsertCallback(0.2f, ()=>
+            sequence.InsertCallback(0.2f, () =>
             {
                  AudioService.Instance.PlaySound(GameAudioType.Progress);
             });
 
             sequence.Append(
-            _progressBar.DOValue(1f, FillDuration)
-                .SetEase(Ease.InOutQuad)
-                .OnComplete(() =>
-                {
+                _progressBar.DOValue(1f, FillDuration)
+                    .SetEase(Ease.InOutQuad)
+            );
 
-                    AudioService.Instance.PlaySound(GameAudioType.Success);
-                    _button.interactable = true;
-                }));
+            sequence.Join(
+                DOVirtual.Float(StartValue, EndValue, FillDuration, (currentValue) =>
+                {
+                    UpdateText(currentValue);
+                }).SetEase(Ease.InOutQuad)
+            );
+
+            sequence.OnComplete(() =>
+            {
+                AudioService.Instance.PlaySound(GameAudioType.Success);
+                _button.interactable = true;
+            });
+        }
+
+        private void UpdateText(float value)
+        {
+            if (value >= 1000f)
+            {
+                _progressText.text = (value / 1000f).ToString("0.#") + "K / 2K";
+            }
+            else
+            {
+                _progressText.text = Mathf.RoundToInt(value).ToString() + " / 2K";
+            }
+        }
+
+        private void OnDestroy()
+        {
+            transform.DOKill();
+            _progressBar.DOKill();
         }
     }
-
 }
