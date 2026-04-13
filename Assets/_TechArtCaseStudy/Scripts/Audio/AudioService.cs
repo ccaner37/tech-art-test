f75c3d776f728f4e03199336ad4ffadf7c85179d
ccaner37 <51262520+ccaner37@users.noreply.github.com>
2026-04-13 18:28:28 +0300

    Fix formatting in AudioService

--- Assets/_TechArtCaseStudy/Scripts/Audio/AudioService.cs
+++ Assets/_TechArtCaseStudy/Scripts/Audio/AudioService.cs
@@ -5,7 +5,7 @@ namespace Appodeal.TechartCaseStudy.AudioEffect
     public class AudioService : MonoBehaviour
     {
         [SerializeField] private AudioSource _audioSource;
-        [SerializeField] private UnityAudioClipSO _audioDatabase; // Assign your SO here in the Inspector
+        [SerializeField] private UnityAudioClipSO _audioDatabase;
 
         public static AudioService Instance { get; private set; }
 
